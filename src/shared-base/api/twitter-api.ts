e4ad0021d03e05d1802a442c0e30b0a2c817abfc
dchagastelles <dchagastelles@gmail.com>
2021-01-03 22:49:18 -0500

    twitter authentication integration in profile

---
+++ src/shared-base/api/twitter-api.ts
@@ -0,0 +1,66 @@
+import request, { BaseplateResp } from 'src/baseplate/request';
+
+
+
+const TwitterApi = {
+
+    GetRequestToken: async () => {
+        let response = await request(`${process.env.REACT_APP_DIDCREDS_URL}/v1/auth/twitter_request`, {
+            method: "GET",
+            headers: {
+                'content-Type': 'application/json',
+                'Authorization': `${process.env.REACT_APP_DIDCREDS_KEY}`,
+            }
+        });
+        // if (response) { // if HTTP-status is 200-299
+        //     // get the response body (the method explained below)
+        //     let json = await response.json();
+        //     return json.data.request_token
+        // }
+
+        return response;
+    }
+
+
+
+    // GetRequestToken: async () => {
+    //     let url = `${process.env.REACT_APP_DIDCRED_URL}/v1/auth/twitter_request`
+    //     let response = request(url, {
+    //         headers: {
+    //             'content-Type': 'application/json',
+    //             "Authorization": process.env.REACT_APP_DIDCRED_KEY
+    //         }
+    //     });
+
+    //     if (response.ok) { // if HTTP-status is 200-299
+    //         // get the response body (the method explained below)
+    //         let json = await response.json();
+    //         return json.data.request_token
+    //     }
+
+    //     return ""
+
+    // },
+    // CallbackTwitter: async (oauth_token, oauth_verifier) => {
+
+    //     let response = await fetch(`${process.env.REACT_APP_DIDCRED_URL}/v1/auth/twitter_callback`, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             "Authorization": process.env.REACT_APP_DIDCRED_KEY
+    //         },
+    //         body: JSON.stringify({
+    //             token: oauth_token,
+    //             verifier: oauth_verifier
+    //         })
+    //     });
+
+    //     if (response.ok) {
+    //         return await response.json()
+    //     }
+
+    //     return null;
+    // }
+}
+
+export default TwitterApi;
