2047e7482caf98e10cd033c83b1c6121654ce087
seniordev83 <denis.vasilev@yahoo.com>
2021-02-12 04:28:01 +1000

    misspelling fixed

--- src/components/ProfileHeader/index.tsx
+++ src/components/ProfileHeader/index.tsx
@@ -19,10 +19,10 @@ const ProfileHeader: React.FC<IProps> = ({ profile }: IProps) => {
   let firstName = profile.profile.firstName || 'Adam';
 
   return (
-    <IonGrid className={style["profileheader"]}>
-      <IonRow className={style["header"]}>
-        <IonCol size="auto">
-          <img src={photo} className={style["profile-img"]} alt="profile" />
+    <IonGrid className={style['profileheader']}>
+      <IonRow className={style['header']}>
+        <IonCol size='auto'>
+          <img src={photo} className={style['profile-img']} alt='profile' />
         </IonCol>
         <IonCol size='8'>
           <IonGrid>
@@ -39,54 +39,77 @@ const ProfileHeader: React.FC<IProps> = ({ profile }: IProps) => {
             </IonRow>
           </IonGrid>
         </IonCol>
-        <IonCol size="2">
-          <img src={edit} alt="edit" />
+        <IonCol size='2'>
+          <img src={edit} alt='edit' />
           <IonProgressBar value={0.78}></IonProgressBar>
-          <span className={style["percent-completed-verified"]}>85% verified</span>
+          <span className={style['percent-completed-verified']}>
+            85% verified
+          </span>
         </IonCol>
       </IonRow>
       <IonRow>
         <IonCol>
-          <div className={style["tip"]}>
+          <div className={style['tip']}>
             <IonGrid>
               <IonRow>
-                <IonCol size="auto">
-                  <img src={bulb} alt="bulb" />
+                <IonCol size='auto'>
+                  <img src={bulb} alt='bulb' />
                 </IonCol>
-                <IonCol size="9">
+                <IonCol size='9'>
                   Recommendation: Connect your DID and publish it to Blockchain
                 </IonCol>
-                <IonCol size="2" className={style["action"]}>Start Service ></IonCol>
+                <IonCol size='2' className={style['action']}>
+                  Start Service
+                </IonCol>
               </IonRow>
             </IonGrid>
           </div>
         </IonCol>
       </IonRow>
       <IonRow>
-        <IonCol><h2>About</h2></IonCol>
+        <IonCol>
+          <h2>About</h2>
+        </IonCol>
       </IonRow>
-      <IonRow className={style["header"]}>
-        <IonCol><p>
-          Experienced Chief Technology Officer with a demonstrated history of working in the financial services industry. Skilled in PHP, Android Development, HTML, Cascading Style Sheets (CSS), and Microsoft PowerPoint. Strong information technology
-          professional with a Bachelor’s degree focused in Computer Software Engineering from University of Management and Technology - UMT.
-          </p></IonCol>
-      </IonRow>
-      <IonRow>
-        <IonCol size="8"><h2>Education</h2></IonCol>
-        <IonCol size="2"><span className={style["addbutton"]}> <img src={addbutton} alt="add" />Add new</span></IonCol>
-        <IonCol size="2">
-          <img src={edit} alt="edit" />
-          <IonProgressBar value={0.78}></IonProgressBar>
-          <span className={style["percent-completed-verified"]}>85% verified</span>
+      <IonRow className={style['header']}>
+        <IonCol>
+          <p>
+            Experienced Chief Technology Officer with a demonstrated history of
+            working in the financial services industry. Skilled in PHP, Android
+            Development, HTML, Cascading Style Sheets (CSS), and Microsoft
+            PowerPoint. Strong information technology professional with a
+            Bachelor’s degree focused in Computer Software Engineering from
+            University of Management and Technology - UMT.
+          </p>
         </IonCol>
       </IonRow>
       <IonRow>
-        <IonCol size="auto"><img src={university} alt="university" /></IonCol>
-        <IonCol size="4" className={style["title-university"]}>University of Management and Technology - UMT</IonCol>
-
-
+        <IonCol size='8'>
+          <h2>Education</h2>
+        </IonCol>
+        <IonCol size='2'>
+          <span className={style['addbutton']}>
+            {' '}
+            <img src={addbutton} alt='add' />
+            Add new
+          </span>
+        </IonCol>
+        <IonCol size='2'>
+          <img src={edit} alt='edit' />
+          <IonProgressBar value={0.78}></IonProgressBar>
+          <span className={style['percent-completed-verified']}>
+            85% verified
+          </span>
+        </IonCol>
       </IonRow>
-
+      <IonRow>
+        <IonCol size='auto'>
+          <img src={university} alt='university' />
+        </IonCol>
+        <IonCol size='4' className={style['title-university']}>
+          University of Management and Technology - UMT
+        </IonCol>
+      </IonRow>
     </IonGrid>
   );
 };
