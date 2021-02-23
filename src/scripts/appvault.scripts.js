819315a1f368c2c58b81424c20e47ecd77664079
Kiran Pachhai <kiran.pachhai@gmail.com>
2021-02-23 15:08:44 -0500

    Fixed an issue with appvault.script.js

--- src/scripts/appvault.scripts.js
+++ src/scripts/appvault.scripts.js
@@ -1,11 +1,11 @@
-const { HiveClient, OptionsBuilder } = require('@elastos/elastos-hive-js-sdk');
-const { ElastosClient } = require('@elastosfoundation/elastos-js-sdk');
-const config = require('./config.json');
-const { testHelper } = require('./testsHelper');
+const { HiveClient, OptionsBuilder } = require("@elastos/elastos-hive-js-sdk");
+const { ElastosClient } = require("@elastosfoundation/elastos-js-sdk");
+const config = require("./config.json");
+const { testHelper } = require("./testsHelper");
 
 let run = async () => {
   let client = await testHelper.getHiveInstance(
-    config.tuum_tech_mnemonic,
+    config.app1,
     config.tuum_tech_mnemonic,
     config.hive_host,
     config.appId
@@ -13,21 +13,21 @@ let run = async () => {
   client.Payment.CreateFreeVault();
 
   // ===== followers section start =====
-  await client.Database.createCollection('followers');
+  await client.Database.createCollection("followers");
   await client.Scripting.SetScript({
-    name: 'set_followers',
+    name: "set_followers",
     executable: {
-      type: 'update',
-      name: 'set_followers',
+      type: "update",
+      name: "set_followers",
       body: {
-        collection: 'followers',
+        collection: "followers",
         filter: {
-          did: '$params.did',
+          did: "$params.did",
         },
         update: {
           $set: {
-            did: '$params.did',
-            followers: '$params.followers',
+            did: "$params.did",
+            followers: "$params.followers",
           },
         },
         options: {
@@ -38,15 +38,15 @@ let run = async () => {
     },
   });
   await client.Scripting.SetScript({
-    name: 'get_followers',
+    name: "get_followers",
     executable: {
-      type: 'find',
-      name: 'get_followers',
+      type: "find",
+      name: "get_followers",
       output: true,
       body: {
-        collection: 'followers',
+        collection: "followers",
         filter: {
-          did: { $in: '$params.did' },
+          did: { $in: "$params.did" },
         },
         options: {
           projection: {
@@ -60,42 +60,42 @@ let run = async () => {
   // ===== followers section end =====
 
   // ===== users section start =====
-  await client.Database.createCollection('users');
+  await client.Database.createCollection("users");
   await client.Scripting.SetScript({
-    name: 'add_user',
+    name: "add_user",
     allowAnonymousUser: true,
     allowAnonymousApp: true,
     executable: {
-      type: 'insert',
-      name: 'add_user',
+      type: "insert",
+      name: "add_user",
       output: true,
       body: {
-        collection: 'users',
+        collection: "users",
         document: {
-          first_name: '$params.first_name',
-          last_name: '$params.last_name',
-          name: '$params.full_name',
-          email: '$params.email',
-          status: '$params.status',
-          code: '$params.code',
-          did: '$params.did',
-          vaulturl: '$params.vaulturl',
+          first_name: "$params.first_name",
+          last_name: "$params.last_name",
+          name: "$params.full_name",
+          email: "$params.email",
+          status: "$params.status",
+          code: "$params.code",
+          did: "$params.did",
+          vaulturl: "$params.vaulturl",
         },
       },
     },
   });
   await client.Scripting.SetScript({
-    name: 'get_users', // by email
+    name: "get_users", // by email
     allowAnonymousUser: true,
     allowAnonymousApp: true,
     executable: {
-      type: 'find',
-      name: 'get_users',
+      type: "find",
+      name: "get_users",
       output: true,
       body: {
-        collection: 'users',
+        collection: "users",
         filter: {
-          email: '$params.email',
+          email: "$params.email",
         },
         options: {
           projection: {
@@ -107,17 +107,17 @@ let run = async () => {
     },
   });
   await client.Scripting.SetScript({
-    name: 'get_users_by_did', // by email
+    name: "get_users_by_did", // by email
     allowAnonymousUser: true,
     allowAnonymousApp: true,
     executable: {
-      type: 'find',
-      name: 'get_users',
+      type: "find",
+      name: "get_users",
       output: true,
       body: {
-        collection: 'users',
+        collection: "users",
         filter: {
-          did: '$params.did',
+          did: "$params.did",
         },
         options: {
           projection: {
@@ -129,58 +129,58 @@ let run = async () => {
     },
   });
   await client.Scripting.SetScript({
-    name: 'update_user_by_did',
+    name: "update_user_by_did",
     executable: {
-      type: 'update',
-      name: 'update_user_by_did',
+      type: "update",
+      name: "update_user_by_did",
       output: true,
       body: {
-        collection: 'users',
+        collection: "users",
         filter: {
-          did: '$params.did',
+          did: "$params.did",
         },
         update: {
           $set: {
-            did: '$params.did',
-            status: '$params.status',
+            did: "$params.did",
+            status: "$params.status",
           },
         },
       },
     },
   });
   await client.Scripting.SetScript({
-    name: 'verify_code',
+    name: "verify_code",
     allowAnonymousUser: true,
     allowAnonymousApp: true,
     executable: {
-      type: 'aggregated',
-      name: 'find_and_update_code',
+      type: "aggregated",
+      name: "find_and_update_code",
       body: [
         {
-          type: 'find',
-          name: 'find_code',
+          type: "find",
+          name: "find_code",
           output: true,
           body: {
-            collection: 'users',
+            collection: "users",
             filter: {
-              code: '$params.code',
-              status: 'WAITING_CONFIRMATION',
+              code: "$params.code",
+              status: "WAITING_CONFIRMATION",
             },
           },
         },
         {
-          type: 'update',
-          name: 'update_status',
+          type: "update",
+          name: "update_status",
           output: false,
           body: {
-            collection: 'users',
+            collection: "users",
             filter: {
-              code: '$params.code',
-              status: 'WAITING_CONFIRMATION',
+              code: "$params.code",
+              status: "WAITING_CONFIRMATION",
             },
             update: {
               $set: {
-                status: 'CONFIRMED',
+                status: "CONFIRMED",
               },
             },
           },
@@ -189,18 +189,18 @@ let run = async () => {
     },
   });
   await client.Scripting.SetScript({
-    name: 'get_users_by_name',
+    name: "get_users_by_name",
     allowAnonymousUser: true,
     allowAnonymousApp: true,
     executable: {
-      type: 'find',
-      name: 'get_users_by_name',
+      type: "find",
+      name: "get_users_by_name",
       output: true,
       body: {
-        collection: 'users',
+        collection: "users",
         filter: {
           // $regex: { name: '.*$params.name.*' },
-          name: { $regex: '$params.name' },
+          name: { $regex: "$params.name" },
           // name: '$params.name',
         },
       },
@@ -210,28 +210,28 @@ let run = async () => {
 
   // ===== universities section start =====
   //store and retrieve universities data from tuum-tech vault
-  await client.Database.createCollection('universities');
-  const fs = require('fs');
+  await client.Database.createCollection("universities");
+  const fs = require("fs");
 
-  fs.readFile('./src/data/world_universities_and_domains.json', (err, data) => {
+  fs.readFile("./src/data/world_universities_and_domains.json", (err, data) => {
     if (err) throw err;
     let universityList = JSON.parse(data);
     console.log(universityList[0]);
-    client.Database.insertMany('universities', universityList);
+    client.Database.insertMany("universities", universityList);
   });
 
   await client.Scripting.SetScript({
-    name: 'get_universities',
+    name: "get_universities",
     allowAnonymousUser: true,
     allowAnonymousApp: true,
     executable: {
-      type: 'find',
-      name: 'get_universities',
+      type: "find",
+      name: "get_universities",
       output: true,
       body: {
-        collection: 'universities',
+        collection: "universities",
         filter: {
-          name: { $regex: '$params.name' },
+          name: { $regex: "$params.name" },
           // $regex: { name: '.*$params.name.*' },
           // name: '$params.name',
         },
@@ -239,7 +239,7 @@ let run = async () => {
     },
   });
   // ===== universities section end =====
-  console.log('All scripts OK');
+  console.log("All scripts OK");
 };
 
 run();
