65243e18102d9e78a504ab304ef191f6351967ea
Ricardo Trapp <ricardo.trapp@gmail.com>
2021-03-11 13:11:24 -0500

    Disable button on step 1 and show continue button

--- src/components/Tutorial/Steps/TutorialStep1.tsx
+++ src/components/Tutorial/Steps/TutorialStep1.tsx
@@ -1,6 +1,7 @@
 import { IonButton } from '@ionic/react';
 import React, { useState } from 'react';
 import ButtonDefault from 'src/components/ButtonDefault';
+import { UserService } from 'src/services/user.service';
 import style from '../style.module.scss'
 export interface ITutorialStepProp{
     onContinue: () => void
@@ -8,12 +9,15 @@ export interface ITutorialStepProp{
 }
 
 const TutorialStep1Component: React.FC<ITutorialStepProp> = ({onContinue}) => {
+
+    const [isDIDPublished] = useState(UserService.GetUserSession().isDIDPublished)
+
     return (
         <div>
             <h2>What is Profile?</h2>
             <p>It's free and easy to get setup</p>
-
-            <IonButton onClick={onContinue} className={style["tutorial-button"]}>Continue</IonButton>
+            {isDIDPublished && (<IonButton onClick={onContinue} className={style["tutorial-button"]}>Continue</IonButton>)}
+            {!isDIDPublished && (<IonButton disabled={true} className={style["tutorial-button"]}>Please wait for DID to be published first</IonButton>)}
         </div>
     );
 }
