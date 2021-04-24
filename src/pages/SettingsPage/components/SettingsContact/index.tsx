808ae742ec52168bc0c0652f2d2440076741ecee
dchagastelles <dchagastelles@gmail.com>
2021-04-24 18:58:21 -0400

    Corrections email, notify toaster

--- src/pages/SettingsPage/components/SettingsContact/index.tsx
+++ src/pages/SettingsPage/components/SettingsContact/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   IonCol,
   IonCard,
@@ -14,8 +14,39 @@ import style from './style.module.scss';
 import TextInput from 'src/components/inputs/TextInput';
 import TextareaInput from 'src/components/inputs/TextareaInput';
 import { Button } from 'src/components/buttons';
+import { UserService } from 'src/services/user.service';
+import { showNotify } from 'src/utils/notify';
 
 const SettingsContact: React.FC = () => {
+  const formRef: any = React.createRef();
+
+  const [subject, setSubject] = useState('');
+  const [description, setDescription] = useState('');
+
+  const send = async () => {
+    const bodyContact = {
+      subject: subject,
+      description: description,
+      email: UserService.GetUserSession()?.loginCred?.email
+    };
+
+    const emailresponse: Response = await fetch(
+      `${process.env.REACT_APP_PROFILE_API_SERVICE_URL}/v1/tuumtech/contact`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${process.env.REACT_APP_PROFILE_API_SERVICE_KEY}`
+        },
+        body: JSON.stringify(bodyContact)
+      }
+    );
+
+    if (emailresponse.status === 200) {
+      showNotify('Email sent successfully', 'success');
+    }
+  };
+
   return (
     <IonContent className={style['settingscontact']}>
       <IonGrid className={style['tab-grid']}>
@@ -27,29 +58,40 @@ const SettingsContact: React.FC = () => {
                 <PageTitle>Contact Us[UNDER CONSTRUCTION]</PageTitle>
               </IonCardHeader>
               <IonCardContent>
-                <IonText>
-                  <p>
-                    Terms and conditions (also referred to as terms of use or
-                    terms of service) are a form of legal agreement outlining
-                    rules and restrictions for customers to follow when using
-                    your site
-                  </p>
-                </IonText>
-                <TextInput
-                  label="Subject"
-                  onChange={n => n}
-                  placeholder="Enter your title"
-                ></TextInput>
-                <TextareaInput
-                  label="Description"
-                  cols={20}
-                  rows={6}
-                  onChange={n => n}
-                  placeholder="Write your message..."
-                ></TextareaInput>
-                <SpaceLs></SpaceLs>
-                <Button type="primary" text="Send Message"></Button>
-                <SpaceMd></SpaceMd>
+                <form
+                  ref={formRef}
+                  action={`process.env.REACT_APP_PROFILE_API_SERVICE_URL/v1/tuumtech/support`}
+                >
+                  <IonText>
+                    <p>
+                      Terms and conditions (also referred to as terms of use or
+                      terms of service) are a form of legal agreement outlining
+                      rules and restrictions for customers to follow when using
+                      your site
+                    </p>
+                  </IonText>
+                  <TextInput
+                    label="Subject"
+                    value={subject}
+                    onChange={(e: any) => setSubject(e)}
+                    placeholder="Enter your title"
+                  ></TextInput>
+                  <TextareaInput
+                    label="Description"
+                    cols={20}
+                    rows={6}
+                    value={description}
+                    onChange={(e: any) => setDescription(e)}
+                    placeholder="Write your message..."
+                  ></TextareaInput>
+                  <SpaceLs></SpaceLs>
+                  <Button
+                    type="primary"
+                    text="Send Message"
+                    onClick={send}
+                  ></Button>
+                  <SpaceMd></SpaceMd>
+                </form>
               </IonCardContent>
             </IonCard>
           </IonCol>
--- src/pages/SettingsPage/components/SettingsReport/index.tsx
+++ src/pages/SettingsPage/components/SettingsReport/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   IonCol,
   IonCard,
@@ -16,8 +16,59 @@ import SelectInput from 'src/components/inputs/SelectInput';
 import TextareaInput from 'src/components/inputs/TextareaInput';
 import FileInput from 'src/components/inputs/FileInput';
 import { Button } from 'src/components/buttons';
+import { UserService } from 'src/services/user.service';
+import { showNotify } from 'src/utils/notify';
 
 const SettingsReport: React.FC = () => {
+  const [comments, setComments] = useState('');
+  const [subject, setSubject] = useState('');
+  const [feedback, setFeedback] = useState('');
+
+  let fileToSend: any = null;
+
+  const toBase64 = (file: File) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+
+  const setFile = async (file: File) => {
+    const fileBase64 = await toBase64(file);
+    // using nodemailer attachments structure so no need to convert anything
+    fileToSend = {
+      fileName: file.name,
+      path: fileBase64
+    };
+  };
+
+  const send = async () => {
+    const bodyContact = {
+      subject: subject,
+      feedback_type: feedback === '1' ? 'Suggestion' : 'Bug',
+      comments: comments,
+      email: UserService.GetUserSession()?.loginCred?.email,
+      attachments: fileToSend
+    };
+
+    const emailResponse: Response = await fetch(
+      `${process.env.REACT_APP_PROFILE_API_SERVICE_URL}/v1/tuumtech/support`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${process.env.REACT_APP_PROFILE_API_SERVICE_KEY}`
+        },
+        body: JSON.stringify(bodyContact)
+      }
+    );
+
+    if (emailResponse.status === 200) {
+      showNotify('Email sent successfully', 'success');
+    }
+  };
+
   return (
     <IonContent className={style['settingsreport']}>
       <IonGrid className={style['tab-grid']}>
@@ -43,24 +94,33 @@ const SettingsReport: React.FC = () => {
                     { value: 1, text: 'suggestion' },
                     { value: 2, text: 'bug' }
                   ]}
-                  onChange={() => {}}
+                  onChange={e => setFeedback(e)}
                   placeholder="Suggestion"
                 ></SelectInput>
                 <TextInput
                   label="Subject"
-                  onChange={n => n}
+                  value={subject}
+                  onChange={(e: any) => setSubject(e)}
                   placeholder="Give it a short name"
                 ></TextInput>
                 <TextareaInput
                   label="Comments"
                   cols={20}
                   rows={6}
-                  onChange={n => n}
+                  value={comments}
+                  onChange={(e: any) => setComments(e)}
                   placeholder="Write your message here..."
                 ></TextareaInput>
-                <FileInput label="Attachment" onChange={n => n}></FileInput>
+                <FileInput
+                  label="Attachment"
+                  onChange={e => setFile(e)}
+                ></FileInput>
                 <SpaceLs></SpaceLs>
-                <Button type="primary" text="Send Message"></Button>
+                <Button
+                  type="primary"
+                  text="Send Message"
+                  onClick={send}
+                ></Button>
               </IonCardContent>
             </IonCard>
           </IonCol>
