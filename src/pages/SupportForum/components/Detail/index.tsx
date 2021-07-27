41949c33c5d41b28ad0fc5b7a9f5f89505155673
eratos <37297413+Eratos1122@users.noreply.github.com>
2021-07-27 15:09:24 +0900

    detail page change

--- src/pages/SupportForum/components/Detail/index.tsx
+++ src/pages/SupportForum/components/Detail/index.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { IonSearchbar } from '@ionic/react';
+import { IonList, IonItem, IonLabel, IonAvatar } from '@ionic/react';
 import styled from 'styled-components';
 
 import LeftArrow from 'src/elements/arrows/LeftArrow';
-import SelectInput from 'src/elements/inputs/SelectInput';
 import { timeSince } from 'src/utils/time';
 
 import TextareaInput from 'src/elements/inputs/TextareaInput';
 import { DefaultButton } from 'src/elements/buttons';
 import { TableContent, Category } from '../common';
+
+import voteIcon from 'src/assets/icon/arrow-up-filled.svg';
+import photo from 'src/assets/icon/dp.png';
 
 interface DetailProp {
   githubIssue: any;
@@ -57,6 +59,8 @@ const Issue = styled.div`
     }
 
     .vote {
+      display: flex;
+      align-items: center;
       font-weight: 700;
       font-size: 14px;
       line-height: 20px;
@@ -87,6 +91,9 @@ const Issue = styled.div`
         margin-bottom: 15px;
       }
     }
+    .comment-history {
+      margin-top: 30px;
+    }
   }
 `;
 
@@ -98,7 +105,7 @@ const Containr = styled.div`
 const Detail: React.FC<DetailProp> = ({ githubIssue }) => {
   return (
     <>
-      <TopBar>
+      <TopBar onClick={() => {}}>
         <LeftArrow fill="#cbd5e0" />
         <p>Back Home</p>
       </TopBar>
@@ -112,7 +119,10 @@ const Detail: React.FC<DetailProp> = ({ githubIssue }) => {
             <span className="category">
               <Category>Bug</Category>
             </span>
-            <span className="vote">1234 votes</span>
+            <span className="vote">
+              <img src={voteIcon} width="15" />
+              1234 votes
+            </span>
             <span className=""></span>
           </div>
           <div className="description">
@@ -140,12 +150,98 @@ const Detail: React.FC<DetailProp> = ({ githubIssue }) => {
                 Share
               </DefaultButton>
             </div>
+            <div className="comment-history">
+              <IonList>
+                <CommentItem>
+                  <IonAvatar slot="start">
+                    <img src={photo} />
+                  </IonAvatar>
+                  <IonLabel>
+                    <h2>Finn</h2>
+                    <h3>
+                      Have his and saying saying all all. Unto saw drakness so
+                      meat divided bring yeras and gathered rule given two in.
+                    </h3>
+                    <p>Listen, I've had a pretty messed up day...</p>
+                  </IonLabel>
+                </CommentItem>
+                <CommentItem>
+                  <IonAvatar slot="start">
+                    <img src={photo} />
+                  </IonAvatar>
+                  <IonLabel>
+                    <h2>Finn</h2>
+                    <h3>
+                      Unto Divide very. Winged. Thing the fish air all sixth
+                      living blessed divide also him every fill over life you'll
+                      without evening bearing moving of.
+                    </h3>
+                    <p>Listen, I've had a pretty messed up day...</p>
+                  </IonLabel>
+                </CommentItem>
+                <CommentItem>
+                  <IonAvatar slot="start">
+                    <img src={photo} />
+                  </IonAvatar>
+                  <IonLabel>
+                    <h2>Finn</h2>
+                    <h3>Green for. Spirit Appear replenish the female fish.</h3>
+                    <p>Listen, I've had a pretty messed up day...</p>
+                  </IonLabel>
+                </CommentItem>
+                <CommentItem>
+                  <IonAvatar slot="start">
+                    <img src={photo} />
+                  </IonAvatar>
+                  <IonLabel>
+                    <h2>Finn</h2>
+                    <h3>
+                      Good female don't cattle beast whales bring upon dominion
+                      for, mroning a living deep abundantly open living. Given
+                      creeping make had from life. Thing form it good earth.
+                    </h3>
+                    <p>Listen, I've had a pretty messed up day...</p>
+                  </IonLabel>
+                </CommentItem>
+              </IonList>
+            </div>
           </div>
         </Issue>
-        <TableContent></TableContent>
+        <TableContent>
+          <div className="table-head">
+            <div className="topic">Topic</div>
+            <div className="category">Category</div>
+            <div className="votes">Votes</div>
+            <div className="date">DATE</div>
+          </div>
+        </TableContent>
       </Containr>
     </>
   );
 };
+
+const CommentItem = styled(IonItem)`
+  display: flex;
+  align-items: center;
+  --border-color: #ffffff;
+
+  ion-label {
+    h2 {
+      color: #27272e;
+      font-weight: bold;
+      font-size: 13px;
+      line-height: 162.02%;
+    }
+    h3 {
+      olor: #27272e;
+      font-size: 16px;
+      line-height: 162.02%;
+    }
+    p {
+      font-size: 13px;
+      line-height: 162.02%;
+    }
+  }
+`;
 
 export default Detail;
