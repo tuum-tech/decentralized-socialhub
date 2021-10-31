896bd9c39e39e05eb31b191a51e472bfaeffb350
commonlot <denis.vasilev@yahoo.com>
2021-10-31 11:25:42 -0500

    side menu fixed

--- src/components/layouts/LeftSideMenu/index.tsx
+++ src/components/layouts/LeftSideMenu/index.tsx
@@ -11,6 +11,16 @@ import { InferMappedProps, SubState } from './types';
 
 import { UserService } from 'src/services/user.service';
 
+import ConnectionMenu from './ConnectionMenu';
+import {
+  HouseSvg,
+  PeopleSvg,
+  SearchSvg,
+  SettingsSvg,
+  ActivitySvg,
+  SignOutSvg
+} from './icons';
+
 import style from './style.module.scss';
 
 const LeftSideMenu: React.FC<InferMappedProps> = ({
@@ -25,154 +35,42 @@ const LeftSideMenu: React.FC<InferMappedProps> = ({
         <IonList>
           <IonItem
             className={
-              // active === 'dashboard'
               history.location.pathname === '/profile'
                 ? style['item-active']
                 : style['item-link']
             }
             onClick={() => history.push('/profile')}
           >
-            <IonIcon
-              slot="start"
-              src="../../assets/icon_dashboard.svg"
-              className={style['img']}
-            ></IonIcon>
+            <HouseSvg />
             <IonLabel>
               <h3>Dashboard</h3>
             </IonLabel>
           </IonItem>
           <IonItem
             className={
-              // active === 'profile_manager'
               history.location.pathname === '/manager'
                 ? style['item-active']
                 : style['item-link']
             }
             onClick={async () => history.push('/manager')}
           >
-            <IonIcon
-              slot="start"
-              src="../../assets/icon_profile_manager.svg"
-              className={style['img']}
-            ></IonIcon>
+            <PeopleSvg />
             <IonLabel>
               <h3>Profile Manager</h3>
             </IonLabel>
           </IonItem>
-          <IonItem
-            disabled={props.session.tutorialStep !== 4}
-            className={
-              // active === 'connections-followers' ||
-              // active === 'connections-followings'
-              history.location.pathname === '/connections/followers' ||
-              history.location.pathname === '/connections/followings'
-                ? style['item-active']
-                : style['item-link']
-            }
-            onClick={async () => history.push('/connections/followers')}
-          >
-            <IonIcon
-              slot="start"
-              src="../../assets/icon_connections.svg"
-              className={style['img']}
-              title={
-                props.session.tutorialStep === 4
-                  ? ''
-                  : 'Please complete the tutorial to access your Connections'
-              }
-            ></IonIcon>
-            <IonLabel
-              title={
-                props.session.tutorialStep === 4
-                  ? ''
-                  : 'Please complete the tutorial to access your Connections'
-              }
-            >
-              <h3>Connections</h3>
-            </IonLabel>
-          </IonItem>
 
-          {history.location.pathname.includes('/connections/') && (
-            <>
-              <IonItem
-                className={
-                  // active === 'connections-followers'
-                  history.location.pathname === '/connections/followers'
-                    ? style['item-active'] +
-                      ' ' +
-                      style['item-connections-active']
-                    : style['item-link'] + ' ' + style['item-connections-link']
-                }
-                onClick={async () => history.push('/connections/followers')}
-              >
-                <IonIcon
-                  slot="start"
-                  src="../../assets/icon_connections.svg"
-                  className={style['img']}
-                ></IonIcon>
-                <IonLabel>
-                  <h3>Followers</h3>
-                </IonLabel>
-              </IonItem>
-              <IonItem
-                className={
-                  // active === 'connections-followings'
-                  history.location.pathname === '/connections/followings'
-                    ? style['item-active'] +
-                      ' ' +
-                      style['item-connections-active']
-                    : style['item-link'] + ' ' + style['item-connections-link']
-                }
-                onClick={async () => history.push('/connections/followings')}
-              >
-                <IonIcon
-                  slot="start"
-                  src="../../assets/icon_connections.svg"
-                  className={style['img']}
-                ></IonIcon>
-                <IonLabel>
-                  <h3>Followings</h3>
-                </IonLabel>
-              </IonItem>
-              <IonItem
-                className={
-                  // active === 'connections-followings'
-                  history.location.pathname === '/connections/mutual-followers'
-                    ? style['item-active'] +
-                      ' ' +
-                      style['item-connections-active']
-                    : style['item-link'] + ' ' + style['item-connections-link']
-                }
-                onClick={async () =>
-                  history.push('/connections/mutual-followers')
-                }
-              >
-                <IonIcon
-                  slot="start"
-                  src="../../assets/icon_connections.svg"
-                  className={style['img']}
-                ></IonIcon>
-                <IonLabel>
-                  <h3>Mutual Followers</h3>
-                </IonLabel>
-              </IonItem>
-            </>
-          )}
+          <ConnectionMenu session={props.session} />
 
           <IonItem
             className={
-              // active === 'search' ?
               history.location.pathname === '/explore'
                 ? style['item-active']
                 : style['item-link']
             }
             onClick={async () => history.push('/explore')}
           >
-            <IonIcon
-              slot="start"
-              src="../../assets/icon_search.svg"
-              className={style['img']}
-            ></IonIcon>
+            <SearchSvg />
             <IonLabel>
               <h3>Explore</h3>
             </IonLabel>
@@ -180,43 +78,32 @@ const LeftSideMenu: React.FC<InferMappedProps> = ({
           <hr className={style['divider']} />
           <IonItem
             className={
-              // active === 'settings'
               history.location.pathname === '/settings'
                 ? style['item-active']
                 : style['item-link']
             }
             onClick={async () => history.push('/settings')}
           >
-            <IonIcon
-              slot="start"
-              src="../../assets/icon_settings.svg"
-              className={style['img']}
-            ></IonIcon>
+            <SettingsSvg />
             <IonLabel>
               <h3>Settings</h3>
             </IonLabel>
           </IonItem>
           <IonItem
             className={
-              // active === 'settings'
               history.location.pathname === '/activities'
                 ? style['item-active']
                 : style['item-link']
             }
             onClick={async () => history.push('/activities')}
           >
-            <IonIcon
-              slot="start"
-              src="../../assets/icon_activities.svg"
-              className={style['img']}
-            ></IonIcon>
+            <ActivitySvg />
             <IonLabel>
               <h3>Activities</h3>
             </IonLabel>
           </IonItem>
           {/* <IonItem
             className={
-              // active === 'notifications'
               history.location.pathname === '/notifications'
                 ? style['item-active']
                 : style['item-link']
@@ -232,7 +119,7 @@ const LeftSideMenu: React.FC<InferMappedProps> = ({
               <h3>Notifications</h3>
             </IonLabel>
           </IonItem> */}
-          <IonItem
+          {/* <IonItem
             className={
               history.location.pathname === '/support-forum'
                 ? style['item-active']
@@ -248,18 +135,14 @@ const LeftSideMenu: React.FC<InferMappedProps> = ({
             <IonLabel>
               <h3>Support Forum</h3>
             </IonLabel>
-          </IonItem>
+          </IonItem> */}
           <IonItem
             className={style['item-link']}
             onClick={() => {
               UserService.logout();
             }}
           >
-            <IonIcon
-              slot="start"
-              src="../../assets/sign-out.svg"
-              className={style['img']}
-            ></IonIcon>
+            <SignOutSvg />
             <IonLabel>
               <h3>Sign Out</h3>
             </IonLabel>
@@ -269,9 +152,7 @@ const LeftSideMenu: React.FC<InferMappedProps> = ({
           <img src="../../assets/icon_profile_manager.svg" /> Profile Manager<br />
           <img src="../../assets/icon_connections.svg" /> Connections<br />
           <img src="../../assets/icon_explore.svg" /> Explore<br /> */}
-
         {/* <hr /> */}
-
         {/* <img src="../../assets/icon_settings.svg" /> Settings<br />
           <img src="../../assets/icon_notifications.svg" /> Notifications<br />           */}
       </div>
@@ -279,7 +160,6 @@ const LeftSideMenu: React.FC<InferMappedProps> = ({
   );
 };
 
-// export default LeftSideMenu;
 export const mapStateToProps = createStructuredSelector<SubState, SubState>({
   session: makeSelectSession()
 });
