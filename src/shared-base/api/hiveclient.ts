c85e83d0af74e899d78f09a10eec80aeb9a0486b
dchagastelles <dchagastelles@gmail.com>
2022-04-27 10:55:26 -0400

    add hive client and hive context provider

---
+++ src/shared-base/api/hiveclient.ts
@@ -0,0 +1,334 @@
+import {
+  VaultServices,
+  ScriptingService,
+  DatabaseService,
+  VaultSubscriptionService,
+  AppContext,
+  AppContextParameters,
+  ServiceContext,
+  DIDResolverAlreadySetupException
+} from '@elastosfoundation/hive-js-sdk/';
+
+import { Logger } from '../logger';
+import { HiveContextProvider } from './hivecontextprovider';
+import { DIDDocument } from '@elastosfoundation/did-js-sdk/';
+import { CacheManager } from '../cachemanager';
+import { AnonymousScriptingService } from 'src/services/anonymous.scripting.service';
+
+export class HiveClientParameters {
+  public context = {} as AppContextParameters;
+  public hiveHost = '';
+  public resolverUrl = '';
+  public resolverCache = '';
+}
+
+const environmentParameters: HiveClientParameters = {
+  hiveHost: process.env.REACT_APP_HIVE_HOST as string,
+  resolverUrl: process.env.REACT_APP_HIVE_RESOLVER_URL as string,
+  resolverCache: process.env.REACT_APP_HIVE_CACHE_DIR as string,
+  context: {
+    storePath: process.env.REACT_APP_APPLICATION_STORE_PATH,
+    appDID: process.env.REACT_APP_APPLICATION_DID,
+    appMnemonics: process.env.REACT_APP_APPLICATION_MNEMONICS,
+    appPhrasePass: process.env.REACT_APP_APPLICATION_PASSPHRASE,
+    appStorePass: process.env.REACT_APP_APPLICATION_STORE_PASS,
+    userDID: '',
+    userMnemonics: '',
+    userPhrasePass: '',
+    userStorePass: ''
+  } as AppContextParameters
+};
+
+export class HiveClient {
+  private static APP_INSTANCE_DOCUMENT_CACHE_KEY = 'APP_INSTANCE_DOCUMENT';
+  private static LOG = new Logger('HiveClient');
+  private databaseService?: DatabaseService;
+  private scriptingService?: ScriptingService;
+  private anonymousScriptingService?: AnonymousScriptingService;
+  private vaultSubscriptionService?: VaultSubscriptionService;
+  private vaultServices?: VaultServices;
+  private anonymous: boolean;
+  private hiveClientParameters: HiveClientParameters;
+  private appContext: AppContext;
+
+  private constructor(
+    anonymous: boolean,
+    appContext: AppContext,
+    hiveClientParameters: HiveClientParameters,
+    vaultServices?: VaultServices,
+    vaultSubscriptionService?: VaultSubscriptionService
+  ) {
+    HiveClient.LOG.debug(
+      'Creating HiveClient instance with {} ...',
+      JSON.stringify(appContext)
+    );
+    this.anonymous = anonymous;
+    this.appContext = appContext;
+    this.hiveClientParameters = hiveClientParameters;
+    if (!anonymous) {
+      this.databaseService = vaultServices?.getDatabaseService();
+      this.scriptingService = vaultServices?.getScriptingService();
+      this.vaultSubscriptionService = vaultSubscriptionService;
+      this.vaultServices = vaultServices;
+    } else {
+    }
+  }
+
+  get Database(): DatabaseService {
+    HiveClient.LOG.trace('Database');
+    if (!this.databaseService)
+      throw new Error('HiveClient: Authentication required.');
+    return this.databaseService;
+  }
+
+  get Vault(): VaultServices {
+    HiveClient.LOG.trace('Vault');
+    if (!this.vaultServices)
+      throw new Error('HiveClient: Authentication required.');
+    return this.vaultServices;
+  }
+
+  get VaultSubscription(): VaultSubscriptionService {
+    HiveClient.LOG.trace('VaultSubscription');
+    if (!this.vaultSubscriptionService)
+      throw new Error('HiveClient: Authentication required.');
+    return this.vaultSubscriptionService;
+  }
+
+  get Scripting(): ScriptingService {
+    HiveClient.LOG.trace('Scripting');
+    if (!this.anonymous && !this.scriptingService)
+      throw new Error('HiveClient: Authentication required.');
+    if (this.anonymous && !this.anonymousScriptingService)
+      throw new Error('HiveClient: Anonymous Scripting Service unavailable.');
+    return this.anonymous
+      ? this.anonymousScriptingService!
+      : this.scriptingService!;
+  }
+
+  public isAnonymous(): boolean {
+    HiveClient.LOG.trace('isAnonymous');
+    return this.anonymous;
+  }
+
+  public static async createAnonymousInstance(
+    hiveHost: string
+  ): Promise<HiveClient> {
+    HiveClient.LOG.trace('createAnonymousInstance');
+    let hiveClient: HiveClient = CacheManager.get('HiveClient', hiveHost);
+
+    if (!hiveClient) {
+      HiveClient.LOG.debug('Creating new anonymous HiveClient instance...');
+      let appContextParameters = environmentParameters;
+      appContextParameters.hiveHost = hiveHost;
+      let instanceAppContextParameters = HiveClient.resolveDefaultParameters(
+        appContextParameters
+      );
+      HiveClient.LOG.debug(
+        'Initializing anonymous resolver with {} and {} ...',
+        instanceAppContextParameters.resolverUrl,
+        instanceAppContextParameters.resolverCache
+      );
+      try {
+        AppContext.setupResolver(
+          instanceAppContextParameters.resolverUrl,
+          instanceAppContextParameters.resolverCache
+        );
+      } catch (e) {
+        if (!(e instanceof DIDResolverAlreadySetupException)) {
+          throw e;
+        }
+      }
+      HiveClient.LOG.debug(
+        'Building anonymous Hive context with {} ...',
+        JSON.stringify(instanceAppContextParameters)
+      );
+      let appContext = await HiveClient.buildAnonymousAppContext(
+        instanceAppContextParameters
+      );
+      hiveClient = new HiveClient(
+        true,
+        appContext,
+        instanceAppContextParameters
+      );
+      HiveClient.LOG.debug('New anonymous HiveClient created.');
+      hiveClient.setAnonymousScriptingService(hiveHost);
+      CacheManager.set('HiveClient', hiveHost, hiveClient);
+    }
+    return hiveClient;
+  }
+
+  public static async createInstance(
+    appContextParameters?: HiveClientParameters
+  ): Promise<HiveClient> {
+    HiveClient.LOG.trace('createInstance');
+    let hiveClient = CacheManager.get('HiveClient', appContextParameters);
+
+    if (!hiveClient) {
+      HiveClient.LOG.debug('Creating new HiveClient instance...');
+      let instanceAppContextParameters = HiveClient.resolveDefaultParameters(
+        appContextParameters
+      );
+      HiveClient.LOG.debug(
+        'Initializing resolver with {} and {} ...',
+        instanceAppContextParameters.resolverUrl,
+        instanceAppContextParameters.resolverCache
+      );
+      try {
+        AppContext.setupResolver(
+          instanceAppContextParameters.resolverUrl,
+          instanceAppContextParameters.resolverCache
+        );
+      } catch (e) {
+        if (!(e instanceof DIDResolverAlreadySetupException)) {
+          throw e;
+        }
+      }
+      HiveClient.LOG.debug(
+        'Building Hive context with {} ...',
+        JSON.stringify(instanceAppContextParameters)
+      );
+      let appContext = await HiveClient.buildAppContext(
+        instanceAppContextParameters
+      );
+      hiveClient = new HiveClient(
+        false,
+        appContext,
+        instanceAppContextParameters,
+        new VaultServices(appContext, instanceAppContextParameters.hiveHost),
+        new VaultSubscriptionService(
+          appContext,
+          instanceAppContextParameters.hiveHost
+        )
+      );
+      HiveClient.LOG.debug('New HiveClient created.');
+      CacheManager.set('Hiveclient', appContextParameters, hiveClient);
+    }
+    return hiveClient;
+  }
+
+  public static async getHiveVersion(hiveHost: string): Promise<string> {
+    HiveClient.LOG.trace('getHiveVersion');
+    let hiveClient = await HiveClient.createAnonymousInstance(hiveHost);
+
+    let serviceContext = new ServiceContext(
+      hiveClient.appContext,
+      hiveClient.hiveClientParameters.hiveHost
+    );
+
+    return (await serviceContext.getNodeVersion()).toString();
+  }
+
+  private static resolveDefaultParameters(
+    hiveClientParameters?: HiveClientParameters
+  ): HiveClientParameters {
+    HiveClient.LOG.trace('resolveDefaultParameters');
+    if (!hiveClientParameters) return environmentParameters;
+    for (const defaultPropertyKey in environmentParameters) {
+      const key = defaultPropertyKey as keyof HiveClientParameters;
+      if (key == 'context') {
+        if (!hiveClientParameters.context) {
+          hiveClientParameters.context = environmentParameters.context;
+        } else {
+          let appContextParameters = hiveClientParameters.context;
+          for (const defaultContextPropertyKey in environmentParameters.context) {
+            const contextKey = defaultContextPropertyKey as keyof AppContextParameters;
+            if (!appContextParameters[contextKey]) {
+              appContextParameters[contextKey] =
+                environmentParameters.context[contextKey];
+            }
+          }
+        }
+      } else {
+        if (!hiveClientParameters[key]) {
+          hiveClientParameters[key] = environmentParameters[key];
+        }
+      }
+    }
+    return hiveClientParameters;
+  }
+
+  private static async buildAnonymousAppContext(
+    appContextParameters: HiveClientParameters
+  ): Promise<AppContext> {
+    HiveClient.LOG.trace('buildAnonymousAppContext');
+    return await AppContext.build(
+      {
+        getLocalDataDir(): string {
+          HiveClient.LOG.trace('HiveAnonymousContextProvider: getLocalDataDir');
+          return appContextParameters.context.storePath;
+        },
+
+        /**
+         * The method for upper Application to implement to provide current application
+         * instance did document as the running context.
+         * @return The application instance did document.
+         */
+        async getAppInstanceDocument(): Promise<DIDDocument> {
+          HiveClient.LOG.trace(
+            'HiveAnonymousContextProvider: getAppInstanceDocument'
+          );
+          return new Promise((resolve, reject) => {
+            resolve(new DIDDocument());
+          });
+        },
+
+        /**
+         * The method for upper Application to implement to acquire the authorization
+         * code from user's approval.
+         * @param authenticationChallengeJWtCode  The input challenge code from back-end node service.
+         * @return The credential issued by user.
+         */
+        getAuthorization(
+          authenticationChallengeJWtCode: string
+        ): Promise<string> {
+          HiveClient.LOG.trace(
+            'HiveAnonymousContextProvider: getAuthorization'
+          );
+          return new Promise((resolve, reject) => {
+            resolve('');
+          });
+        }
+      },
+      ''
+    );
+  }
+
+  private static async buildAppContext(
+    appContextParameters: HiveClientParameters
+  ): Promise<AppContext> {
+    HiveClient.LOG.trace('buildAppContext');
+    return await AppContext.build(
+      await HiveContextProvider.create(appContextParameters.context),
+      appContextParameters.context.userDID
+    );
+  }
+
+  public isConnected(): boolean {
+    HiveClient.LOG.trace('isConnected');
+    return this.isConnected() || this.getAccessToken() ? true : false;
+  }
+
+  public getAccessToken(): string | null {
+    HiveClient.LOG.trace('getAccessToken');
+    try {
+      return this.Vault.getAccessToken().getJwtCode();
+    } catch (e) {
+      HiveClient.LOG.error(e);
+    }
+    return null;
+  }
+
+  public setAnonymousScriptingService(hiveHost: string): void {
+    HiveClient.LOG.trace('setAnonymousScriptingService');
+    HiveClient.LOG.debug(
+      "setAnonymousScriptingService for '{}' using context: {}",
+      hiveHost,
+      JSON.stringify(this.appContext)
+    );
+    let serviceContext = new ServiceContext(this.appContext, hiveHost);
+    this.anonymousScriptingService = new AnonymousScriptingService(
+      serviceContext
+    );
+  }
+}
---
+++ src/shared-base/api/hivecontextprovider.ts
@@ -0,0 +1,322 @@
+import {
+  Claims,
+  DID,
+  DIDDocument,
+  DIDStore,
+  DIDURL,
+  Issuer,
+  JWTHeader,
+  JWTParserBuilder,
+  RootIdentity,
+  VerifiableCredential,
+  VerifiablePresentation
+} from '@elastosfoundation/did-js-sdk/';
+import {
+  HiveException,
+  AppContextProvider,
+  AppContextParameters
+} from '@elastosfoundation/hive-js-sdk/';
+import dayjs from 'dayjs';
+import { DidService } from 'src/services/did.service.new';
+import { HiveContextProviderException } from '../exceptions';
+import { DID as CNDID } from '@elastosfoundation/elastos-connectivity-sdk-js';
+import { Logger } from '../logger';
+
+export class HiveContextProvider implements AppContextProvider {
+  private static LOG = new Logger('HiveContextProvider');
+  private contextParameters: AppContextParameters;
+
+  private appRootId?: RootIdentity;
+  private userRootId?: RootIdentity;
+  private store?: DIDStore;
+
+  constructor(contextParameters: AppContextParameters) {
+    this.contextParameters = contextParameters;
+  }
+
+  public static async create(
+    contextParameters: AppContextParameters
+  ): Promise<AppContextProvider> {
+    HiveContextProvider.LOG.trace('create');
+    let defaultAppContext = new HiveContextProvider(contextParameters);
+
+    await defaultAppContext.init();
+    return defaultAppContext;
+  }
+
+  async init(): Promise<void> {
+    HiveContextProvider.LOG.trace('init');
+    this.store = await DIDStore.open(this.contextParameters.storePath);
+    this.appRootId = await this.initPrivateIdentity(
+      this.contextParameters.appMnemonics,
+      this.contextParameters.appDID,
+      this.contextParameters.appPhrasePass,
+      this.contextParameters.appStorePass
+    );
+    HiveContextProvider.LOG.debug('Init app private identity');
+
+    this.userRootId = await this.initPrivateIdentity(
+      this.contextParameters.userMnemonics,
+      this.contextParameters.userDID,
+      this.contextParameters.userPhrasePass,
+      this.contextParameters.userStorePass
+    );
+    HiveContextProvider.LOG.debug('Init user private identity');
+
+    await this.initDid(this.appRootId);
+    await this.initDid(this.userRootId);
+  }
+
+  public async initPrivateIdentity(
+    mnemonic: string,
+    did: string | DID,
+    phrasePass: string,
+    storePass: string
+  ): Promise<RootIdentity> {
+    HiveContextProvider.LOG.trace('initPrivateIdentity');
+    HiveContextProvider.LOG.debug('Opens store');
+    let id = RootIdentity.getIdFromMnemonic(mnemonic, phrasePass);
+
+    HiveContextProvider.LOG.debug('ID from mnemonic {} : {}', mnemonic, id);
+
+    if (this.store!.containsRootIdentity(id)) {
+      HiveContextProvider.LOG.debug('Store constains RootIdentity');
+      return await this.store!.loadRootIdentity(id);
+    }
+
+    let rootIdentity: RootIdentity;
+    try {
+      HiveContextProvider.LOG.info(
+        'Creating root identity for mnemonic {}',
+        mnemonic
+      );
+      rootIdentity = RootIdentity.createFromMnemonic(
+        mnemonic,
+        phrasePass,
+        this.store!,
+        storePass
+      );
+    } catch (e) {
+      HiveContextProvider.LOG.error(
+        'Error Creating root identity for mnemonic {}. Error {}',
+        mnemonic,
+        JSON.stringify(e)
+      );
+      throw new Error('Error Creating root identity for mnemonic');
+    }
+
+    await rootIdentity.synchronize();
+    rootIdentity.setDefaultDid(did);
+
+    return rootIdentity;
+  }
+
+  public async initDid(rootIdentity: RootIdentity): Promise<void> {
+    HiveContextProvider.LOG.trace('initDid');
+    let dids = await this.store!.listDids();
+    if (dids.length > 0) {
+      this.contextParameters.appDID = dids[0];
+      return;
+    }
+    HiveContextProvider.LOG.debug('Init app did');
+
+    let did = await rootIdentity.getDefaultDid();
+    let resolvedDoc = await did.resolve();
+    await this.store!.storeDid(resolvedDoc);
+    HiveContextProvider.LOG.debug('Resolve app doc');
+  }
+
+  getLocalDataDir(): string {
+    HiveContextProvider.LOG.trace('getLocalDataDir');
+    return this.contextParameters.storePath;
+  }
+
+  /**
+   * The method for upper Application to implement to provide current application
+   * instance did document as the running context.
+   * @return The application instance did document.
+   */
+  async getAppInstanceDocument(): Promise<DIDDocument> {
+    HiveContextProvider.LOG.trace('getAppInstanceDocument');
+    return await this.getAppDocument();
+  }
+
+  /**
+   * The method for upper Application to implement to acquire the authorization
+   * code from user's approval.
+   * @param authenticationChallengeJWtCode  The input challenge code from back-end node service.
+   * @return The credential issued by user.
+   */
+  async getAuthorization(
+    authenticationChallengeJWtCode: string
+  ): Promise<string> {
+    HiveContextProvider.LOG.trace('getAuthorization');
+    try {
+      let claims: Claims = (
+        await new JWTParserBuilder()
+          .build()
+          .parse(authenticationChallengeJWtCode)
+      ).getBody();
+      if (claims == null)
+        throw new HiveException('Invalid jwt token as authorization.');
+
+      HiveContextProvider.LOG.debug('getAuthorization createPresentation');
+
+      let presentation = this.contextParameters.userMnemonics
+        ? await this.generateVerifiablePresentationFromUserMnemonics(
+            this.contextParameters.userMnemonics,
+            this.contextParameters.userPhrasePass,
+            claims.getIssuer(),
+            claims.get('nonce') as string
+          )
+        : await this.generateVerifiablePresentationFromEssentialCred(
+            claims.getIssuer(),
+            claims.get('nonce') as string
+          );
+
+      //TestData.LOG.debug("TestData->presentation: " + presentation.toString(true));
+      return await this.createToken(presentation, claims.getIssuer());
+    } catch (e) {
+      HiveContextProvider.LOG.error(
+        'TestData->getAuthorization error: {} stack {}',
+        e,
+        e.stack
+      );
+      throw new HiveContextProviderException(e.message);
+    }
+  }
+
+  // Copied from did.service.new.ts
+  private getExpirationDate() {
+    HiveContextProvider.LOG.trace('getExpirationDate');
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth();
+    var day = d.getDate();
+    var c = new Date(year + 5, month, day);
+    return c;
+  }
+
+  // Copied from did.service.new.ts
+  private async generateVerifiablePresentationFromUserMnemonics(
+    userMnemonic: string,
+    password: string,
+    issuer: string,
+    nonce: string
+  ): Promise<any> {
+    HiveContextProvider.LOG.trace(
+      'generateVerifiablePresentationFromUserMnemonics'
+    );
+    let didService = await DidService.getInstance();
+    let appMnemonic = process.env.REACT_APP_APPLICATION_MNEMONICS as string;
+    let appDid = await didService.loadDid(appMnemonic);
+    let userDid = await didService.loadDid(userMnemonic, password);
+
+    let userDocument: DIDDocument = await this.store!.loadDid(userDid);
+
+    // the storePrivateKey method should probably go to loadDid method
+    let id: DIDURL = DIDURL.from('#primary', userDid) as DIDURL;
+    await didService.storePrivatekey(id, userMnemonic, password, 0);
+
+    let id2: DIDURL = DIDURL.from('#primary', appDid) as DIDURL;
+    await didService.storePrivatekey(id2, userMnemonic, password, 0);
+
+    let issuerObject = new Issuer(userDocument, id);
+    let vcBuilder = new VerifiableCredential.Builder(issuerObject, appDid);
+    let vc = await vcBuilder
+      .expirationDate(this.getExpirationDate())
+      .type('AppIdCredential')
+      .property('appDid', appDid.toString())
+      .property('appInstanceDid', appDid.toString())
+      .id(DIDURL.from('#app-id-credential', appDid) as DIDURL)
+      .seal(process.env.REACT_APP_APPLICATION_STORE_PASS as string); // and we sign so it creates a Proof with method and signature
+
+    console.log('isValid: ' + (await vc.isValid()));
+    this.store!.storeCredential(vc);
+
+    let vpb = await VerifiablePresentation.createFor(appDid, null, this.store!);
+    let vp = await vpb
+      .realm(issuer)
+      .nonce(nonce)
+      .credentials(vc)
+      .seal(process.env.REACT_APP_APPLICATION_STORE_PASS as string);
+
+    console.log('vp: ' + vp.toString(true));
+    return vp;
+  }
+
+  // Copied from did.service.new.ts
+  private async generateVerifiablePresentationFromEssentialCred(
+    issuer: string,
+    nonce: string
+  ): Promise<any> {
+    HiveContextProvider.LOG.trace(
+      'generateVerifiablePresentationFromEssentialCred'
+    );
+    let didService = await DidService.getInstance();
+    let appMnemonic = process.env.REACT_APP_APPLICATION_MNEMONICS as string;
+    let appDidInstance = await didService.loadDid(appMnemonic);
+
+    let didAccess = new CNDID.DIDAccess();
+    let vc: VerifiableCredential = await didAccess.generateAppIdCredential();
+
+    this.store!.storeCredential(vc);
+
+    // store private key
+    let id: DIDURL = DIDURL.from('#primary', appDidInstance) as DIDURL;
+    didService.storePrivatekey(id, appMnemonic, '', 0);
+    let response = await didAccess.getExistingAppInstanceDIDInfo();
+    let didStore = await DIDStore.open(response.storeId);
+
+    let vpb = await VerifiablePresentation.createFor(
+      response.didString,
+      null,
+      didStore
+    );
+
+    let vp = await vpb
+      .realm(issuer)
+      .nonce(nonce)
+      .credentials(vc)
+      .seal(response.storePassword);
+
+    return vp;
+  }
+
+  private async getAppDocument(): Promise<DIDDocument> {
+    HiveContextProvider.LOG.trace('getAppDocument');
+    return await this.store!.loadDid(this.contextParameters.appDID);
+  }
+
+  private async createToken(
+    vp: VerifiablePresentation,
+    hiveDid: string
+  ): Promise<string> {
+    HiveContextProvider.LOG.trace('createToken');
+    let cal = dayjs();
+    let iat = cal.unix();
+    let nbf = cal.unix();
+    let exp = cal.add(3, 'month').unix();
+
+    // Create JWT token with presentation.
+    let doc: DIDDocument = await this.getAppDocument();
+    let token = await doc
+      .jwtBuilder()
+      .addHeader(JWTHeader.TYPE, JWTHeader.JWT_TYPE)
+      .addHeader('version', '1.0')
+      .setSubject('DIDAuthResponse')
+      .setAudience(hiveDid)
+      .setIssuedAt(iat)
+      .setExpiration(exp)
+      .setNotBefore(nbf)
+      .claimsWithJson('presentation', vp.toString(true))
+      .sign(this.contextParameters.appStorePass);
+
+    return token;
+  }
+
+  public getAppDid(): DID | null {
+    HiveContextProvider.LOG.trace('getAppDid');
+    return DID.from(this.contextParameters.appDID);
+  }
+}
