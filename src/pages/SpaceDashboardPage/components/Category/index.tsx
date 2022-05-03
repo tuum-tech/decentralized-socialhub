20a829e9387d4e537f830b9cd9cca0ebb78fcd38
fullstack99 <success1989127@gmail.com>
2022-05-03 11:51:33 -0600

    fix merge issue

--- src/pages/SpaceDashboardPage/components/Category/index.tsx
+++ src/pages/SpaceDashboardPage/components/Category/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { IonCardTitle } from '@ionic/react';
+import { IonCardTitle, IonCardContent } from '@ionic/react';
 import Multiselect from 'multiselect-react-dropdown';
 import styled from 'styled-components';
 import {
