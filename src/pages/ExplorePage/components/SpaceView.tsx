07e5c670689790038a49898fb2ce6f1a32991555
eratos <37297413+Eratos1122@users.noreply.github.com>
2022-06-02 01:40:57 +0900

    remove redux to fetch spaces on explore page

--- src/pages/ExplorePage/components/SpaceView.tsx
+++ src/pages/ExplorePage/components/SpaceView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setTimeout, clearTimeout } from 'timers';
 import WelcomeSpace from './WelcomeSpace';
@@ -8,6 +8,7 @@ import LoadingIndicator from 'src/elements/LoadingIndicator';
 import SpaceListView from 'src/components/Space/SpaceListView';
 import { selectSpaces, selectSpacesLoading } from 'src/store/spaces/selectors';
 import { fetchSpaces } from 'src/store/spaces/actions';
+import { SpaceService } from 'src/services/space.service';
 
 interface Props {
   searchKeyword?: string;
@@ -15,8 +16,10 @@ interface Props {
 
 const SpaceView: React.FC<Props> = ({ searchKeyword }: Props) => {
   const dispatch = useDispatch();
-  const loading = useSelector(state => selectSpacesLoading(state));
-  const spaces = useSelector(state => selectSpaces(state));
+  // const loading = useSelector(state => selectSpacesLoading(state));
+  // const spaces = useSelector(state => selectSpaces(state));
+  const [loading, setLoading] = useState(false);
+  const [spaces, setSpaces] = useState<any[]>([]);
 
   const filteredSpaces = useMemo(() => {
     if (searchKeyword) {
@@ -27,13 +30,18 @@ const SpaceView: React.FC<Props> = ({ searchKeyword }: Props) => {
     return spaces;
   }, [spaces, searchKeyword]);
 
-  const refreshSpaces = async () => {
-    dispatch(fetchSpaces());
+  const refreshSpaces = async (anim: boolean = false) => {
+    // dispatch(fetchSpaces());
+    anim && setLoading(true);
+    const _spaces = await SpaceService.getAllSpaces();
+    setSpaces(_spaces);
+    anim && setLoading(false);
   };
 
   useEffect(() => {
-    dispatch(fetchSpaces(true));
-
+    // dispatch(fetchSpaces(true));
+    refreshSpaces(true);
+    
     let timer = setTimeout(function start() {
       refreshSpaces();
       timer = setTimeout(start, 5000);
