53903aee6b62267ccb436cef15cdfe819af382e3
slin <frontdev8@gmail.com>
2022-06-03 15:22:52 -0400

    Update Did card

--- src/utils/did.ts
+++ src/utils/did.ts
@@ -8,3 +8,15 @@ export const getDIDString = (did: string, isLink = true) => {
   }
   return did;
 };
+
+export const getShortenedDid = (did: string, length: number) => {
+  let sDid = did.replace('did:elastos:', '');
+  if (sDid.length <= 2 * length) {
+    return did;
+  }
+  sDid = `${sDid.substring(0, length)}...${sDid.substring(
+    sDid.length - length
+  )}`;
+
+  return `did:elastos:${sDid}`;
+};
